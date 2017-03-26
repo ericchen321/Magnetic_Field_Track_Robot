d9c0c86241bc3c77788b4c1ce07ebfffe0f0d734
Eric Chen <Eric Chen>
2017-03-25 22:38:28 -0700

    new commit - transmitter test code and ATMega code

--- Blinky.mk
+++ Blinky.mk
@@ -1,19 +1,19 @@
 # Specify the compiler to use
 CC=c51
 # Object files to link
-OBJS=BlinkyISR.obj
+OBJS=Blinky.obj
 
-# The default 'target' (output) is BlinkyISR.hex and 'depends' on
+# The default 'target' (output) is Blinky.hex and 'depends' on
 # the object files listed in the 'OBJS' assignment above.
-# These object files are linked together to create BlinkyISR.hex.
-BlinkyISR.hex: $(OBJS)
+# These object files are linked together to create Blinky.hex.
+Blinky.hex: $(OBJS)
 	$(CC) $(OBJS)
 	@echo Done!
 
-# The object file BlinkyISR.o depends on BlinkyISR.c. BlinkyISR.c is compiled
-# to create BlinkyISR.obj.
-BlinkyISR.obj: BlinkyISR.c
-	$(CC) -c BlinkyISR.c
+# The object file Blinky.o depends on Blinky.c. Blinky.c is compiled
+# to create Blinky.o.
+Blinky.obj: Blinky.c
+	$(CC) -c Blinky.c
 
 # Target 'clean' is used to remove all object files and executables
 # associated wit this project
@@ -22,14 +22,15 @@ clean:
 
 # Target 'FlashLoad' is used to load the hex file to the microcontroller 
 # using the flash loader.  If the folder of the flash loader has been
-# added to 'PATH' just 'Prog89lp' is needed.  Otherwise, a valid path
+# added to 'PATH' just 'F38X_prog' is needed.  Otherwise, a valid path
 # must be provided as shown below.
 LoadFlash:
-	Pro89lp.exe -p -v BlinkyISR.hex
+	F38X_prog Blinky.hex
 
 # Phony targets can be added to show useful files in the file list of
 # CrossIDE or execute arbitrary programs:
-Dummy: BlinkyISR.hex BlinkyISR.Map
+Dummy: Blinky.hex Blinky.Map
 	
 explorer:
 	explorer .
+		
